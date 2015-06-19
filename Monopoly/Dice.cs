be45227a33cc450be1a84f6f768cb84e05f1cfb2
jsavage06 <jon5@vt.edu>
2015-06-19 14:02:04 -0400

    initial

---
+++ Monopoly/Dice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    public class Dice
+    {
+        private const int DEFAULT_DICE = 2;
+        private const int DEFAULT_DICE_SIDES = 6;
+
+        private readonly int numberOfDiceToRoll;
+        private readonly int numberOfDiceSides;
+
+        public Dice(int diceCount = DEFAULT_DICE, int sideCount = DEFAULT_DICE_SIDES)
+        {
+            numberOfDiceToRoll = diceCount;
+            numberOfDiceSides = sideCount;
+        }
+
+        public int Roll()
+        {
+            return Enumerable.Sum(Enumerable.Repeat(0, numberOfDiceToRoll).Select(i => Die.RollDie(numberOfDiceSides)));
+        }
+
+        private static class Die
+        {
+            private const int DEFAULT_SIDES = 6;
+            private static Random random = new Random();
+
+            public static int RollDie(int sides = DEFAULT_SIDES)
+            {
+                return random.Next(sides) + 1;
+            }
+        }
+    }
+}
---
+++ MonopolyUnitTests/DiceUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Monopoly;
+
+namespace MonopolyUnitTests
+{
+    
+    [TestFixture]
+    class DiceUnitTests
+    {
+        Dice dice;
+
+        [Test]
+        public void Roll_Two_Dice_Six_Sided_Score_Is_GTE_2_LTE_12()
+        {
+            dice = new Dice();
+
+            foreach (var rollValue in Enumerable.Repeat(0, 100).Select(i => dice.Roll()))
+            {
+                Assert.GreaterOrEqual(rollValue, 2);
+                Assert.LessOrEqual(rollValue, 12);
+            }
+        }
+    }
+}
