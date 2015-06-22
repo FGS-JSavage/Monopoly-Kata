70ade857f175e7c644f46c45df0049188bcd5f80
jsavage06 <jon5@vt.edu>
2015-06-22 16:42:01 -0400

    Implemented LandOnGoTask, all tests pass

---
+++ Monopoly/Tasks/LandOnGoTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    public class LandOnGoTask : IPlayerTask
+    {
+        public void Complete(IPlayer player)
+        {
+            player.Balance += 200;
+        }
+    }
+}
---
+++ Monopoly/Tasks/PayIncomeTaxTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly.Tasks
+{
+    public class PayIncomeTaxTask : IPlayerTask
+    {
+        public void Complete(IPlayer player)
+        {
+            // TODO
+        }
+    }
+}
