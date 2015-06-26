f3ded68e0aa82a494ab6a65f64c96bdc55e718ea
jsavage06 <jon5@vt.edu>
2015-06-26 16:13:32 -0400

    In the process of refactoring before starting release 5

---
+++ Monopoly/Interfaces/IPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    public interface IPlayer
+    {
+        ILocation PlayerLocation    { get; set; }
+        double Balance              { get; set; }
+
+        void CompleteLandOnLocationTasks();
+        void CompleteExitLocationTasks();
+    }
+}
