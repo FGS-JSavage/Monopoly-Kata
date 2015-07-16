a2019810e767cfcbe385af48456918da4ac948a4
jsavage06 <jon5@vt.edu>
2015-07-16 13:23:06 -0400

    removed onLandOnTasks

--- Monopoly/Locations/Location.cs
+++ Monopoly/Locations/Location.cs
@@ -9,7 +9,6 @@ namespace Monopoly
     public class Location : ILocation
     {
         protected List<IPlayerTask> OnLandTasks = new List<IPlayerTask>();
-        protected List<IPlayerTask> OnExitTasks = new List<IPlayerTask>();
 
         public int SpaceNumber     { get; set; }
         public PropertyGroup Group { get; set; }
@@ -23,11 +22,6 @@ namespace Monopoly
         public List<IPlayerTask> GetOnLandTasks()
         {
             return OnLandTasks;
-        }
-
-        public List<IPlayerTask> GetOnExitTasks()
-        {
-            return OnExitTasks;
         }
     }
 }
