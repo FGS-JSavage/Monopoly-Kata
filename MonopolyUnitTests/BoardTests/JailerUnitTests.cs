bce7c2646fb2a2206a8ed29de6153c96e58bcf0f
jsavage06 <jon5@vt.edu>
2015-07-21 17:09:41 -0400

    Added JailerUnitTests

---
+++ MonopolyUnitTests/BoardTests/JailerUnitTests.cs
@@ -0,0 +1,72 @@
+﻿using Moq;
+using Monopoly;
+using Monopoly.Board;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+
+namespace MonopolyUnitTests.BoardTests
+{
+    [TestFixture]
+    class JailerUnitTests
+    {
+        private Mock<Player> mockPlayer;
+        private Jailer jailer;
+
+        [SetUp]
+        public void Init()
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+            mockPlayer = fixture.Create<Mock<Player>>();
+            jailer = new Jailer();
+        }
+
+        [Test]
+        public void PlayerIsNotInPrison_PlayerIsImprisonedReturnsFalse()
+        {
+            Assert.False(jailer.PlayerIsImprisoned(mockPlayer.Object));
+        }
+
+
+        [Test]
+        public void ThePlayerIsImprisoned_PlayerIsImprisonedReturnsTrue()
+        {
+            jailer.Imprison(mockPlayer.Object);
+           
+            Assert.True(jailer.PlayerIsImprisoned(mockPlayer.Object));
+        }
+
+        [Test]
+        public void PlayerIsImprisoned_PlayerStartsWithA3RoundSentence()
+        {
+            int startingSentence = 3;
+
+            jailer.Imprison(mockPlayer.Object);
+
+            Assert.AreEqual(startingSentence, jailer.GetRemainingSentence(mockPlayer.Object));
+        }
+
+        [Test]
+        public void PlayerIsImprisoned_CallDecreaseSentence_SentenceIsDecrementedByOne()
+        {
+            int startingSentence = 3;
+
+            jailer.Imprison(mockPlayer.Object);
+
+            jailer.DecreaseSentence(mockPlayer.Object);
+
+            Assert.AreEqual(startingSentence - 1, jailer.GetRemainingSentence(mockPlayer.Object));
+        }
+
+        [Test]
+        public void ThePlayerIsImprisoned_PlayerIsReleasedFromJail_PlayerIsImprisonedReturnsFalse()
+        {
+            jailer.Imprison(mockPlayer.Object);
+
+            jailer.ReleasePlayerFromJail(mockPlayer.Object);
+
+            Assert.False(jailer.PlayerIsImprisoned(mockPlayer.Object));
+        }
+    }
+}
