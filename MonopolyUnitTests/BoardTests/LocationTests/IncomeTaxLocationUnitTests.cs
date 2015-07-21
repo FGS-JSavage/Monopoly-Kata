f014be0c2ab59077c9c8cafb612daf193e839098
jsavage06 <jon5@vt.edu>
2015-07-21 13:28:05 -0400

    Addeds IncomeTaxLocationUnitTests

---
+++ MonopolyUnitTests/BoardTests/LocationTests/IncomeTaxLocationUnitTests.cs
@@ -0,0 +1,42 @@
+﻿using Monopoly.Board;
+using Monopoly.Board.Locations;
+using Monopoly.Tasks;
+using NUnit.Framework;
+
+namespace MonopolyUnitTests.BoardTests.LocationTests
+{
+    [TestFixture]
+    class IncomeTaxLocationUnitTests
+    {
+        private IncomeTaxLocation incomeTaxLocation;
+
+        [SetUp]
+        public void Init()
+        {
+            incomeTaxLocation = new IncomeTaxLocation();
+        }
+
+
+        [Test]
+        public void IncomeTaxLocationInitializesWithCorrectSpaceNumber()
+        {
+            var expectedSpaceNumber = 4;
+
+            Assert.AreEqual(incomeTaxLocation.SpaceNumber, expectedSpaceNumber);
+        }
+
+        [Test]
+        public void IncomeTaxLocationInitializesWithCorrectTasks()
+        {
+            Assert.True(incomeTaxLocation.GetOnLandTasks()[0].GetType() == typeof(PayIncomeTaxTask));
+        }
+
+        [Test]
+        public void IncomeTaxLocationInitializesWithCorrectGroup()
+        {
+            var expectedGroup = PropertyGroup.Tax;
+
+            Assert.AreEqual(incomeTaxLocation.Group, expectedGroup);
+        }
+    }
+}
