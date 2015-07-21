0dcfaa317e3ddb8ee6cc3f0d58c26fdae78fa987
jsavage06 <jon5@vt.edu>
2015-07-21 13:42:02 -0400

    AddedJailVisitingLocationUnitTests

---
+++ MonopolyUnitTests/BoardTests/LocationTests/JailVisitingLocationUnitTests.cs
@@ -0,0 +1,40 @@
+﻿using Monopoly.Board;
+using Monopoly.Board.Locations;
+using NUnit.Framework;
+
+namespace MonopolyUnitTests.BoardTests.LocationTests
+{
+    [TestFixture]
+    class JailVisitingLocationUnitTests
+    {
+        private JailVisitingLocation jailVisitingLocation;
+
+        [SetUp]
+        public void Init()
+        {
+            jailVisitingLocation = new JailVisitingLocation();
+        }
+
+        [Test]
+        public void JailVisitingLocationInitializesWithCorrectSpaceNumber()
+        {
+            var expectedSpaceNumber = 10;
+
+            Assert.AreEqual(jailVisitingLocation.SpaceNumber, expectedSpaceNumber);
+        }
+
+        [Test]
+        public void JailVisitingLocationInitializesWithCorrectTasks()
+        {
+            Assert.True(jailVisitingLocation.GetOnLandTasks().Count == 0);
+        }
+
+        [Test]
+        public void JailVisitingLocationInitializesWithCorrectGroup()
+        {
+            var expectedGroup = PropertyGroup.JailVisiting;
+
+            Assert.AreEqual(jailVisitingLocation.Group, expectedGroup);
+        }
+    }
+}
